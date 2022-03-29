d3400988563c61cf834aba87fc203e90e6f7d73e
= <=>
2022-03-29 11:56:54 +0000

    Add Dark Mode

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-const dark = () => document.querySelector('html').classList.toggle('dark')
+const dark = document.querySelector('#dark');
+const html = document.querySelector('html');
+const sunIcon = document.getElementById('sun-icon');
+const moonIcon = document.getElementById('moon-icon');
+
+
+dark.addEventListener('click', () => {
+    html.classList.toggle('dark');
+    sunIcon.classList.toggle('hidden');
+    moonIcon.classList.toggle('hidden');
+})
